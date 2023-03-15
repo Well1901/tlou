6b4a32519f271aad7352bfa33e49f4c77263e99e
Well1901 <127906263+Well1901@users.noreply.github.com>
2023-03-14 21:05:16 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,44 @@
+/*
+    OBJETIVO - quando clicarmos no botão temos que mostrar a imagem de funco correspondente
+
+    - passo 1 - dar um jeito de pegar o elemento HTML dos botões
+
+    - passo 2 - dar um jeito de identificar o clique do usuário no botão
+
+    - passo 3 - desmarcar o botão selecionado anterior
+
+    - passo 4 - marcar o botão clicando como se estivesse selecionado
+
+    - passo 5 - esconder a imagem de fundo anterior
+
+    - passo 6- fazer aparecer a imagem de fundo correspondente ao botão clicado
+*/
+
+// - passo 1 - dar um jeito de pegar o elemento HTML dos botões
+const botoesCarrossel = document.querySelectorAll('.botao');
+const imagens = document.querySelectorAll('.imagem');
+const textos = document.querySelectorAll('.descricao');
+
+//- passo 2 - dar um jeito de identificar o clique do usuário no botão
+botoesCarrossel.forEach((botao, indice) => {
+    botao.addEventListener('click', () =>{
+       //- passo 3 - desmarcar o botão selecionado anterior
+       const botaoSelecionado = document.querySelector('.selecionado');
+       botaoSelecionado.classList.remove('selecionado');
+
+       //- passo 4 - marcar o botão clicando como se estivesse selecionado
+       botao.classList.add('selecionado');
+
+       //- passo 5 - esconder a imagem ativa de fundo
+        const imagemAtiva = document.querySelector('.ativa');
+        imagemAtiva.classList.remove('ativa');
+
+        //- passo 6- fazer aparecer a imagem de fundo correspondente ao botão clicado
+        imagens[indice].classList.add('ativa');
+
+        const textosAtivo = document.querySelector('.ativo');
+        textosAtivo.classList.remove('ativo');
+
+        descricao.classList.add('ativo');
+    })
+})
